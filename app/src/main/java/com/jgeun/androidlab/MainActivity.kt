1e2db4337dbc948e0bb3a858f9e8ec9514c66fb1
jgeun <pjk8926@gmail.com>
2023-09-16 18:43:47 +0900

    Set Home Navigation

--- app/src/main/java/com/jgeun/androidlab/MainActivity.kt
+++ app/src/main/java/com/jgeun/androidlab/MainActivity.kt
@@ -2,10 +2,18 @@ package com.jgeun.androidlab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.jgeun.androidlab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+	private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+	private val navHostFragment by lazy {
+		supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+	}
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		setContentView(binding.root)
 	}
 }
