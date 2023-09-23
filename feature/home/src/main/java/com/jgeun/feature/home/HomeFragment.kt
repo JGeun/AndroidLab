ae70d94b68423b8a2fcc4450aab54afb5c8230f0
jgeun <pjk8926@gmail.com>
2023-09-23 23:46:18 +0900

    Apply Compose in Home

--- feature/home/src/main/java/com/jgeun/feature/home/HomeFragment.kt
+++ feature/home/src/main/java/com/jgeun/feature/home/HomeFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
 import com.jgeun.feature.home.databinding.FragmentHomeBinding
 
@@ -17,6 +20,15 @@ class HomeFragment : Fragment() {
 		savedInstanceState: Bundle?
 	): View {
 		_binding = FragmentHomeBinding.inflate(inflater, container, false)
+		binding.homeComposeView.apply {
+			setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+			setContent {
+				// In Compose world
+				MaterialTheme {
+					Text("Hello Compose!")
+				}
+			}
+		}
 		return binding.root
 	}
 
